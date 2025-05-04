0f7ffa7d8af0526cf4939ec78b2ffd0751510ee6
Rajendra kishore <67578904+Rajendrakishore01@users.noreply.github.com>
2025-05-04 05:09:24 +0000

    datatypesbasics

---
+++ .devcontainer/basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3)  we are using node js not broswer thats why it shows an error of undefined alert.
+
+// orignal documentation of java script is avliabile on tc.39.es/ecma262
+
+let name =" Rajendra kishore " //string data type
+let age= 20  //number data type
+let isloggenIN = true // boolean data type
+/*  number => 2^53
+bigint
+string => ""
+boolean => true/false
+null=> standalone value (object)
+undefined =>  type iska undefined hi hota h
+symbol => uniqueness ke liye use krte h , react ,figma me bahut use hota h
+
+
+*/
+console.log(typeof(age)) // used to check the data type
