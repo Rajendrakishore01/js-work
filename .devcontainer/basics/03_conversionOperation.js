8d7058a704aba4f335d7f1fb84b96a781fe95be2
Rajendra kishore <67578904+Rajendrakishore01@users.noreply.github.com>
2025-05-04 05:25:40 +0000

    conversion of datatype

---
+++ .devcontainer/basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score ="18"
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+/* 
+    ...............NOTES ......................
+"18"=> 18
+"18abc"=> NaN {not a number}
+true => 1;
+false => 0;
+1=> true;
+0=> false;
+""=> false
+"Rajendra"=> true
+
+
+*/
+
+let isloggenIn="Rajendra"
+let booleanIsLoggedIn= boolean(isloggenIn)
+console.log(booleanIsLoggedIn);
+
+
+// conversion from Number to String
+let someNumber=18
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
