5851afa313311d4d8750f5bebe89aa483489f1ec
Rajendra kishore <67578904+Rajendrakishore01@users.noreply.github.com>
2025-05-04 05:42:59 +0000

    conversion and operations done

--- .devcontainer/basics/03_conversionOperation.js
+++ .devcontainer/basics/03_conversionOperation.js
@@ -31,3 +31,45 @@ let someNumber=18
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// *******************************************Operations*****************************************************
+
+
+let value =3
+let negValue= -value
+console.log(negValue); // it convert the value in negative , output:=> -3
+
+// some basics operations
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+
+//add two string
+let str1="Hello"
+let str2=" Rajendra"
+
+let str3= str1+ str2
+console.log(str3)
+
+// Important conversion 
+
+console.log("1"+2); // output: 12
+console.log(1+"2"); //output: 12
+console.log("1"+2+2); // output : 122
+console.log(1+2+"2"); // output: 32
+
+console.log(+true); // gives 1 output
+console.log(+"");
+
+// prefix and postfix conversion
+let gameCounter =100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter)
+
+// study this topic from here=> https://tc39.es/ecma262/#sec-abstract-operations
